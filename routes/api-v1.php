c09be7b8d10aeed3a0c5cb008a0a0ffb9fa1f72e
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-01-30 08:50:45 +0100

    agregado endpoints con apiRescource()

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', [RegisterController::class, 'store'])->name('api-v1.register');
+
+//creamos ruta para los endpoints
+// Route::get('categories', [CategoryController::class, 'index'])->name('api.v1.categories.index');
+// Route::post('categories', [CategoryController::class, 'store'])->name('api.v1.categories.store');
+// Route::get('categories/{category}', [CategoryController::class, 'show'])->name('api.v1.categories.show');
+// Route::put('categories/{category}', [CategoryController::class, 'update'])->name('api.v1.categories.update');
+// Route::delete('categories/{category}', [CategoryController::class, 'delete'])->name('api.v1.categories.delete');
+
+//otra manera mas rapida, pero esto no es tipo resource, sino de tipo api
+Route::apiResource('categories', CategoryController::class)->names('api.v1.categories');
